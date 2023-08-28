3b4227417b12e0c1d33b90937a1ed567fe5acec9
Valentyn <wolf.valentyn@gmail.com>
2023-08-28 19:19:49 +0300

    fix html classes, add logo

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
+import todoLogo from './img/todoLogo.svg';
 
 function App() {
   return (
-    <>
+    <div className="todo">
       <header>
-          <img src="./img/todoLogo.svg" alt="todoLogo" />
+          <img src={todoLogo} alt="todoLogo" />
       </header>
-      <div class="main">
-          <div class="main__wrapper">
-              <div class="main__header">
-                  <input type="text" name="todo" placeholder="Add a new Task" id="todo" class="todo-input" />
-                  <button class="add-btn">
+      <div className="todo__main">
+          <div className="todo__main-wrapper">
+              <div className="todo__header">
+                  <input type="text" name="todo" placeholder="Add a new Task" id="todo" className="todo__input" />
+                  <button className="todo__add-btn">
                       Create
-                      <i class="icon-plus"></i>
+                      <i className="icon-plus"></i>
                   </button>
               </div>
-              <div class="counters">
-                  <div class="counters__item counters__created">
+              <div className="todo__counters">
+                  <div className="todo__counters-item todo__counters-item_created">
                       Created Tasks:
                       <span>0</span>
                   </div>
-                  <div class="counters__item counters__done">
+                  <div className="todo__counters-item todo__counters-item_done">
                       Done Tasks:
-                      <div class="counters__done-wrapper">
+                      <div className="todo__counters-item_done_wrapper">
                           <span>0</span> 
-                          <div class="of">of</div> 
+                          <div className="of">of</div> 
                           <span>0</span>
                       </div>    
                   </div>
               </div>
-              <div class="todos"></div>
+              <div className="todo__todos"></div>
           </div>
       </div>
-    </>
+    </div>
   );
 }
 
