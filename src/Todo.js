08680046845e37d562ffb65ec8e635d21209c117
Valentyn <wolf.valentyn@gmail.com>
2023-08-30 14:40:53 +0300

    add onComplete function

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,16 @@
+import { useRef } from "react";
+
 const Todo = ({text, id, onDelete}) => {
+    const spanRef = useRef(null);
+
+    const onComplete = () => {
+        spanRef.current.classList.toggle('completed');
+    } 
+
     return (
         <div className="todo__todos-item">
-            <input type="checkbox" />
-            <span>{text}</span>
+            <input type="checkbox" onChange={onComplete} />
+            <span ref={spanRef}>{text}</span>
             <button onClick={() => onDelete(id)} className="todo__todos-item-delete"></button>
         </div>
     )
