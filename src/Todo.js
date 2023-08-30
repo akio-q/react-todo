ccd1f558c572541da07c53ba44de26c145ac82fb
Valentyn <wolf.valentyn@gmail.com>
2023-08-30 18:23:57 +0300

    change trash icon color when task is complete, small fixes

--- src/Todo.js
+++ src/Todo.js
@@ -2,12 +2,15 @@ import { useRef } from "react";
 
 const Todo = ({text, id, onDelete, onComplete}) => {
     const spanRef = useRef(null);
+    const trashBtnRef = useRef(null);
 
     const handleComplete = (e) => {
         if (e.target.checked) {
             spanRef.current.classList.add('completed');
+            trashBtnRef.current.classList.add('trashBtnHover');
         } else {
             spanRef.current.classList.remove('completed');
+            trashBtnRef.current.classList.remove('trashBtnHover');
         }
 
         onComplete(id, e.target.checked)
@@ -21,7 +24,9 @@ const Todo = ({text, id, onDelete, onComplete}) => {
                 </div>
                 <span ref={spanRef}>{text}</span>
             </div>
-            <button onClick={() => onDelete(id)} className="todo__todos-item-delete"></button>
+            <button ref={trashBtnRef} onClick={() => onDelete(id)} className="todo__todos-item-delete">
+                    <i className="icon-trash"></i>
+            </button>
         </div>
     )
 }
